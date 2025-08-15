dc9b9edd228cffd074cd991b0186f6bea56b8d8a
arifshn <habibarifsahin@gmail.com>
2025-08-15 16:07:32 +0300

    Improvements have been made to stock control

--- DTO/CartDto.cs
+++ DTO/CartDto.cs
@@ -14,5 +14,6 @@ public class CartItemDTO
     public decimal? Price { get; set; }
     public string? ImageUrl { get; set; }
     public int Quantity { get; set; }
+    public int Stock { get; set; }
 
 }
--- obj/Debug/net9.0/API.AssemblyInfo.cs
+++ obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7cbf21e6fc6f2588f33f783f927fdfb93c055c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78cc7ac3dac7d1e941f1f92ef9f3de317472daf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
